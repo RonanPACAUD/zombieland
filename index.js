b7b1626a8c9a52d968c8a890cd0e478d38b7803b
Ronan <ronan.pacaud@gmail.com>
2024-03-25 10:49:57 +0100

    fonctionnel

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 const app = express();
 
-// console.log(path.join(__dirname, './Front/dist'))
-
 app.use(express.static(path.join(__dirname, './Front/dist'))); 
 app.use(express.static(path.join(__dirname, './Front/dist/assets'))); 
 
